e964a74c3f3013a1445d6e23d4badb18feb97753
YCH <YCH@DESKTOP-00071C3>
2018-06-29 13:58:32 +0800

    slider部分

---
+++ src/components/icon-svg/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <svg class="icon" aria-hidden="true">
+        <use :xlink:href="iconName"></use>
+    </svg>
+</template>
+
+<script>
+    export default {
+        name: "icon-svg",
+      data(){
+          return {};
+      },
+      props: {
+          iconClass: {
+              type: String,
+            required: true
+          }
+      },
+      computed:{
+        iconName(){
+            return  `#icon-${this.iconClass}`;
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/components/nav/siderbar/sidebar.vue
+++ src/components/nav/siderbar/sidebar.vue
@@ -1,15 +1,63 @@
 <template>
-
+  <nav>
+      <div class="wq-sidebar" v-bind:class="foldobj">
+          <div class="sidebar-fold" @click="toggleFold">
+              <icon-svg :iconClass="iconObj" class="fold-svg"></icon-svg>
+          </div>
+      </div>
+  </nav>
 </template>
 
 <script>
+  import iconSvg from "@/components/icon-svg";
   export default {
     data(){
-      return{}
+      return{
+
+      }
+
+    },
+    computed:{
+      iconObj(){
+        return this.$store.state.app.siderbar.opened ? "fold" : "unfold";
+      },
+      foldobj(){
+          return this.$store.state.app.siderbar.opened
+            ? "show-sidebar"
+            : "hide-sidebar";
+        }
+    },
+    methods: {
+      toggleFold(){
+
+      }
+    },
+    components:{
+        iconSvg
     }
   }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+  @import "../../../assets/css/common";
+  .wq-sidebar{
+    height: 100%;
+  }
+  .sidebar-fold{
+    cursor: pointer;
+    text-align: center;
+    height: 28px;
+  }
+  .fold-svg{
+    height: 28px;
+  }
+  .hide-sidebar {
+    width: 64px;
+  }
+  .show-sidebar {
+    min-width: 200px;
+  }
+  .hide-sidebar {
+    min-width: 64px;
+  }
 </style>
